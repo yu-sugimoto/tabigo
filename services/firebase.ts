6aac5039e263fe08182fc7fb45c3de10a02d02e5
hirossan4049 <haruto405329@gmail.com>
2025-02-24 04:07:57 +0900

    add firebase.ts

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,19 +1,30 @@
-// import firebase from 'firebase/app';
-// import 'firebase/auth';
-// import 'firebase/database';
+import { getApps, initializeApp } from 'firebase/app';
+import 'firebase/auth';
+import { getAuth, initializeAuth } from 'firebase/auth';
+//@ts-ignore
+import { getReactNativePersistence } from '@firebase/auth/dist/rn/index.js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import 'firebase/database';
+import { getDatabase } from 'firebase/database';
 
-// const firebaseConfig = {
-//   apiKey: 'YOUR_API_KEY',
-//   authDomain: 'YOUR_AUTH_DOMAIN',
-//   databaseURL: 'https://YOUR_DATABASE_URL.firebaseio.com',
-//   projectId: 'YOUR_PROJECT_ID',
-//   storageBucket: 'YOUR_STORAGE_BUCKET',
-//   messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-//   appId: 'YOUR_APP_ID',
-// };
 
-// if (!firebase.apps.length) {
-//   firebase.initializeApp(firebaseConfig);
-// }
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
 
-// export default firebase;
+
+if (!getApps().length) {
+  const app = initializeApp(firebaseConfig);
+  const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage),
+  });
+}
+
+export const auth = getAuth();
+export const database = getDatabase();
