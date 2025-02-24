9ad68eb10f1e46d3ce8918a24bf852b032c29a15
hirossan4049 <haruto405329@gmail.com>
2025-02-24 12:25:27 +0900

    Revert "aa"
    This reverts commit 93d45fcca8a6e6cf897018b45e296bccfcc72fb0.

--- screens/MapScreen/GuideMap.tsx
+++ screens/MapScreen/GuideMap.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // screens/MapScreen/GuideMap.tsx
 import { onValue, ref, set } from 'firebase/database';
 import React, { useEffect, useState } from 'react';
@@ -130,31 +129,3 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 });
-=======
-import MapView, { Polygon } from "react-native-maps";
-
-const GuideMap = () => {
-  const polygonCoords = [
-    { latitude: 35.681236, longitude: 139.767125 },
-    { latitude: 35.685236, longitude: 139.767125 },
-    { latitude: 35.685236, longitude: 139.771125 },
-    { latitude: 35.681236, longitude: 139.771125 },
-  ];
-
-  return (
-    <MapView
-      style={{ width: "100%", height: "100%" }}
-      initialRegion={{
-        latitude: 35.681236,
-        longitude: 139.767125,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-      }}
-    >
-      <Polygon coordinates={polygonCoords} fillColor="rgba(0,0,255,0.2)" />
-    </MapView>
-  );
-}
-
-export default GuideMap;
->>>>>>> d424293 (update)
